e188ff05d6a9bb79b49db508ea3948b20aaf7f09
mehmet_yasar <mehmet.yasar0770@gmail.com>
2024-01-11 16:02:48 +0300

    feat: temel fonksiyonlarıyla uygulama çalıştırıldı

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
   const [text, setText] = useState([]);
 const hadleSubmit = (e) => {
   e.preventDefault();
-  console.log("hello React")
+  let amount = parseInt(count)
+  console.log (amount)
+  setText(data.slice(0,amount))
 }
 
   return (
@@ -15,8 +17,20 @@ const hadleSubmit = (e) => {
       <h3> tired of boring lorem ipsum?</h3>
       <form className="lorem-form" onSubmit={hadleSubmit}> 
       <label htmlFor="amount">paragraphs: </label>
-      <input type="number" name="amount" id="amount" value={count} />
+      <input type="number" name="amount" id="amount" value={count} onChange={(e)=> {
+        setCount (e.target.value)
+      }}/>
+      <button type="submit" className="btn">generate</button>
       </form>
+
+      <article className="lorem-text"> 
+      {text.map ((item,index)=> {
+        return <p key={index}> {item}
+
+        </p>
+
+      })}
+      </article>
     </section>
   );
 }
